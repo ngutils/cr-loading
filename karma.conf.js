055163c7e04c0bd77e556835a2d3d633d5572df3
Gianluca Arbezzano <gianarb92@gmail.com>
2014-05-28 10:23:59 +0200

    Boilerplate my angularModule

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+        "bower_components/angular/angular.js",
+        "bower_components/angular-mocks/angular-mocks.js",
+        "./spec/*.js"
+    ],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  });
+};
